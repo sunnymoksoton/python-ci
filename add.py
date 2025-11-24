259aeec28f92b58508be7ad61b80926d7880ad57
Sunny Mok (ylm1e25) <ylm1e25@soton.ac.uk>
2025-11-24 11:47:59 +0000

    Added raise TypeError again

--- add.py
+++ add.py
@@ -1,6 +1,6 @@
 def add(x: int, y: int) -> int:
-    #if not isinstance(x,int) or not isinstance(y,int):
-     #raise TypeError("Both arguments must be integers")
+    if not isinstance(x,int) or not isinstance(y,int):
+     raise TypeError("Both arguments must be integers")
     """Returns the sum of two integers."""
     return x + y
 
